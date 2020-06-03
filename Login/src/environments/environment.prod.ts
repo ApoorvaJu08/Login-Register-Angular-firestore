04a855457324a03a16a2efe135aae6bc200553a8
ApoorvaJu08 <apoorva10339@gmail.com>
2020-06-03 10:05:35 +0530

    adding project

---
+++ Login/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCgi8Mhoj_Y6F4Kmbirwfv6AqoLAKDp7Ig",
+    authDomain: "practice-project-64244.firebaseapp.com",
+    databaseURL: "https://practice-project-64244.firebaseio.com",
+    projectId: "practice-project-64244",
+    storageBucket: "practice-project-64244.appspot.com",
+    messagingSenderId: "1096573957381",
+    appId: "1:1096573957381:web:bb23334934fb8a915ab36c"
+  }
+};
